2ba3fdfbf4f50eb43e4af9864d1429c519ab6660
Leos <admin@MacBook-Pro-macbook.local>
2018-03-17 22:20:11 +0200

    initial responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,58 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './header.scss';
 
-const Header = () => (
-  <header className="header">
-    <Link className="header__link" to="/">Main</Link>
-    <Link className="header__link" to="/rockets">Rockets</Link>
-    <Link className="header__link" to="/launches">Launches</Link>
-    <Link className="header__link" to="/contacts">Contacts</Link>
-  </header>
-);
+type PropType = {};
+
+type StateType = {
+  isMobileMenuActive: boolean,
+}
+
+class Header extends Component<PropType, StateType> {
+  constructor() {
+    super();
+
+    this.state = {
+      isMobileMenuActive: false,
+    };
+  }
+
+  onHamburgerClick = () => this.toggleMobileMenuActivity();
+  onHamburgerKeyUp = () => this.toggleMobileMenuActivity();
+
+  toggleMobileMenuActivity = () => {
+    this.setState({
+      isMobileMenuActive: !this.state.isMobileMenuActive,
+    });
+  }
+
+  render() {
+    const activeClass = this.state.isMobileMenuActive ? 'active' : '';
+
+    return (
+      <header className="header">
+        <div
+          className={`${activeClass} header__hamburger`}
+          onClick={this.onHamburgerClick}
+          onKeyUp={this.onHamburgerKeyUp}
+          role="menuitem"
+          tabIndex="0"
+        >
+          <span className="header__hamburger-bar" />
+          <span className="header__hamburger-bar" />
+          <span className="header__hamburger-bar" />
+        </div>
+        <nav className={`${activeClass} header__nav`}>
+          <Link className="header__link" to="/">Main</Link>
+          <Link className="header__link" to="/rockets">Rockets</Link>
+          <Link className="header__link" to="/launches">Launches</Link>
+          <Link className="header__link" to="/contacts">Contacts</Link>
+        </nav>
+      </header>
+    );
+  }
+}
 
 export default Header;
--- src/views/contacts/Contacts.jsx
+++ src/views/contacts/Contacts.jsx
@@ -8,7 +8,6 @@ import Copyright from './../../components/copyright/Copyright.jsx';
 function Contacts() {
   return (
     <div>
-      <Header />
       <main className="contacts">
         <a className="contacts__link" href="https://www.linkedin.com/in/adam-leos/" rel="noopener noreferrer" target="_blank">LinkedIn</a>
         <a className="contacts__link contacts__link--facebook" href="https://www.facebook.com/4leos" rel="noopener noreferrer" target="_blank">Facebook</a>
